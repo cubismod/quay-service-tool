d899c193c3132ea7f3db642664236c5a0c50dc26
Ryan Wallace <rywallac@redhat.com>
2026-05-05 10:38:25 -0400

    fix(supervisor): use stdout_logfile instead of supervisor_stdout

--- conf/generate_supervisord_conf.sh
+++ conf/generate_supervisord_conf.sh
@@ -2,16 +2,7 @@
 set -euo pipefail
 
 cd "$(dirname "$0")"
-SERVICETOOL_LOGGING="${SERVICETOOL_LOGGING:-syslog}"
 CONFIG_FILE="./supervisord.conf"
-
-if [ "${SERVICETOOL_LOGGING}" = "stdout" ]; then
-  LOGGING_CMD="command=supervisor_stdout"
-  RESULT_HANDLER="supervisor_stdout:event_handler"
-else
-  LOGGING_CMD="command=supervisor_logging"
-  RESULT_HANDLER=""
-fi
 
 cat << EOF > ${CONFIG_FILE}
 [supervisord]
@@ -22,19 +13,8 @@ environment=
   PYTHONPATH=%(ENV_SERVICETOOLDIR)s
 command=gunicorn -k gevent -b 0.0.0.0:5000 --limit-request-field_size 16384 app:app
 autostart = true
-stdout_events_enabled = true
-stderr_events_enabled = true
-
-[eventlistener:stdout]
-environment=
-  PYTHONPATH=%(ENV_SERVICETOOLDIR)s
-buffer_size = 1024
-events = PROCESS_LOG
-${LOGGING_CMD}
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
 EOF
-
-
-# Add result_handler only for stdout logging
-if [ -n "${RESULT_HANDLER}" ]; then
-  echo "result_handler=${RESULT_HANDLER}" >> ${CONFIG_FILE}
-fi
